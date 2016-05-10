eff91f0bef866c953f802daacf9bdaa6cc138760
Roger Howard <rogerhoward@gmail.com>
2016-05-09 23:31:45 -0700

    Working

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
+ffmpeg = '/usr/local/bin/ffmpeg'
 presets = {}
 
 presets['seventwenty'] = {
-    'template': 'ffmpeg -i {input} -vf scale=-1:360 -c:v libx264 -crf 22 -c:a copy {output}'
+    'template': '/usr/local/bin/ffmpeg -i {input} -y -vf scale=-1:360 -c:v libx264 -crf 22 -c:a copy {output}'
 }
 
 presets['default'] = presets['seventwenty']
--- ffse.py
+++ ffse.py
@@ -1,51 +1,95 @@
 #!/usr/bin/env python
-
+import re
 import config
 import click
-import os
 import subprocess
+from pathlib import Path
+
+from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
+    FileTransferSpeed, FormatLabel, Percentage, \
+    ProgressBar, ReverseBar, RotatingMarker, \
+    SimpleProgress, Timer, AdaptiveETA, AbsoluteETA, AdaptiveTransferSpeed
 
 class Encoder(object):
-
     def convert_subprocess(self):
-        print('convert_subprocess: {}'.format(self.command))
         process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
         for line in process.stdout:
-            print(line)
+            line = line.strip()
+
+            if line.startswith('frame='):
+                try:
+                    self.frame = int(re.match(r'frame=\s*(\d+)', line).group(1))
+                except:
+                    pass
+
+            self.progress()
+
+    def progress(self):
+        self.progressbar.update(int(self.status * 100))
 
     @property
     def filename(self):
-        return os.path.basename(self.original)
+        return Path(self.original).name
 
     @property
     def output(self):
-        new_filename = '{}_{}.{}'.format(os.path.splitext(self.filename)[0], self.preset, self.extension)
-        if dir:
-            return os.path.join(self.destination, new_filename)
+        original = Path(self.original)
+        new_filename = '{}_{}.{}'.format(original.stem, self.preset, self.extension)
+        if self.destination:
+            destination = Path(self.destination)
+            return destination / new_filename
         else:
-            return os.path.join(os.path.dirname(self.original), new_filename)
+            return original.parent / new_filename
+
 
     @property
     def command(self):
-
         data = {'input': self.original, 'output': self.output}
         command_string = config.presets[self.preset]['template'].format(**data)
-        print('command_string: {}'.format(command_string))
-
+        # print('command_string: {}'.format(command_string))
         return command_string
 
+    @staticmethod
+    def _get_ffmpeg_info(path):
+        process = subprocess.Popen([config.ffmpeg, "-i", path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, _ = process.communicate()
+        return stdout.decode()
+
+    @property
+    def resolution(self):
+        match_dict = re.search(r"\s(?P<h>\d+?)x(?P<v>\d+?)\s", self.ffmpeg_info).groupdict()
+        return {key: int(match_dict[key]) for key in match_dict}
+
+    @property
+    def frame_rate(self):
+        match_dict = re.search(r"\s(?P<fps>[\d\.]+?)\stbr", self.ffmpeg_info).groupdict()
+        return float(match_dict["fps"])
+
+    @property
+    def duration(self):
+        match_dict = re.search(r"Duration:\s(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),",
+                               self.ffmpeg_info).groupdict()
+        return float(match_dict["hours"])*60**2\
+               + float(match_dict["minutes"])*60\
+               + float(match_dict["seconds"])
+
+    @property
+    def status(self):
+        value = self.frame / (self.duration * self.frame_rate)
+        if value > 1:
+            value = 1.0
+        return value
+
     def __init__(self, dir, preset, file):
-        print(dir, preset, file)
+        self.frame = 0
         self.original = file
         self.preset = preset
         self.destination = dir
         self.extension = 'mp4'
-        print('init complete')
+        self.ffmpeg_info = self._get_ffmpeg_info(self.original)
 
     def __str__(self):
-        return self.original
-        # return '{} -> {}'.format(self.filename, os.path.basename(self.output))
-
+        return self.filename
 
 
 @click.command()
@@ -55,11 +99,13 @@ class Encoder(object):
 def handle(dir, preset, file):
     click.echo('Converting {} using preset "{}"'.format(file, preset))
     this_encoder = Encoder(dir, preset, file)
-    print('this_encoder: {}'.format(this_encoder))
-    print('this_encoder.output: {}'.format(this_encoder.output))
-    print('this_encoder.command: {}'.format(this_encoder.command))
-    this_encoder.convert_subprocess()
 
+    print('duration: {}'.format(this_encoder.duration))
+    print('frame rate: {}'.format(this_encoder.frame_rate))
+    print('frames: {}'.format(this_encoder.duration * this_encoder.frame_rate))
+
+    with ProgressBar(max_value=100) as this_encoder.progressbar:
+        this_encoder.convert_subprocess()
 
 
 if __name__ == '__main__':
