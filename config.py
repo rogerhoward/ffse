da49bb3f98373a8436c90ea06796137276b7ffea
Roger Howard <rogerhoward@gmail.com>
2016-05-10 10:04:45 -0700

    Added lots of docstrings

--- config.py
+++ config.py
@@ -2,12 +2,28 @@
 FFMPEG_PATH = '/usr/local/bin/ffmpeg'
 presets = {}
 
+"""
+Presets can contain the following named substitutions:
+    in:  the input filepath
+    out: the output filepath
+"""
+
+presets['threesixty'] = {
+    'template': '-i {in} -y -vf scale=-1:360 -c:v libx264 -crf 22 -c:a copy {out}',
+    'extension': 'mp4',
+    'description': '',
+}
+
 presets['seventwenty'] = {
-    'template': '-i {in} -y -vf scale=-1:360 -c:v libx264 -crf 22 -c:a copy {out}'
+    'template': '-i {in} -y -vf scale=-1:720 -c:v libx264 -crf 22 -c:a copy {out}',
+    'extension': 'mp4',
+    'description': '',
 }
 
 presets['seventwentyshort'] = {
-    'template': '-i {in} -y -vf scale=-1:360 -ss 5 -t 10 -c:v libx264 -crf 22 -c:a copy {out}'
+    'template': '-i {in} -y -vf scale=-1:360 -ss 5 -t 10 -c:v libx264 -crf 22 -c:a copy {out}',
+    'extension': 'mp4',
+    'description': '',
 }
 
-presets['default'] = presets['seventwenty']
+presets['default'] = presets['threesixty']
--- ffse.py
+++ ffse.py
@@ -16,6 +16,10 @@ from utils import colorize as clrz
 
 class Encoder(object):
     def convert(self):
+        """
+        Actually runs the encoder.
+        Currently uses Subprocess synchronously, but may switch to sh and Celery.
+        """
         process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
         for line in process.stdout:
             line = line.strip()
@@ -29,6 +33,9 @@ class Encoder(object):
             self.progress()
 
     def progress(self):
+        """
+        Updates Encoders progressbar display.
+        """
         if int(self.frame) > self.frames:
             self.progressbar.update(int(self.frames))
         else:
@@ -37,12 +44,19 @@ class Encoder(object):
 
     @property
     def filename(self):
+        """
+        Filename of input file.
+        """
         return Path(self.original).name
 
     @property
     def output(self):
+        """
+        Absolute path to output file.
+        """
         original = Path(self.original)
-        new_filename = '{}_{}.{}'.format(original.stem, self.preset, self.extension)
+        data = {'stem': original.stem, 'preset': self.preset_name, 'ext': self.preset['extension']}
+        new_filename = '{stem}_{preset}.{ext}'.format(**data)
         if self.destination:
             destination = Path(self.destination)
             return str(destination / new_filename)
@@ -51,27 +65,48 @@ class Encoder(object):
 
     @property
     def command(self):
+        """
+        Returns fully formatted command line string for processing input
+        file according to the selected preset.
+        """
         data = {'in': self.original, 'out': self.output}
-        options_string = config.presets[self.preset]['template'].format(**data)
+        options_string = self.preset['template'].format(**data)
         command_string = '{} {}'.format(config.FFMPEG_PATH, options_string)
         return command_string
 
     @property
     def frames(self):
+        """
+        Returns number of frames in the input file, for showing progress.
+        Value is calculated from duration and framerate retrieved from
+        result of _get_ffmpeg_info()
+        """
         return int(self.duration * self.frame_rate)
 
     @property
     def resolution(self):
+        """
+        Returns pixel dimensions of input file as a tuple.
+        Resolution extracted from result of _get_ffmpeg_info()
+        """
         match_dict = re.search(r"\s(?P<h>\d+?)x(?P<v>\d+?)\s", self.ffmpeg_info).groupdict()
         return {key: int(match_dict[key]) for key in match_dict}
 
     @property
     def frame_rate(self):
+        """
+        Returns framerate of input file as a float number of frames per second.
+        Framerate extracted from result of _get_ffmpeg_info()
+        """
         match_dict = re.search(r"\s(?P<fps>[\d\.]+?)\stbr", self.ffmpeg_info).groupdict()
         return float(match_dict["fps"])
 
     @property
     def duration(self):
+        """
+        Returns length of input file as a float number of seconds.
+        Duration extracted from result of _get_ffmpeg_info()
+        """
         match_dict = re.search(r"Duration:\s(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),",
                                self.ffmpeg_info).groupdict()
         return float(match_dict["hours"])*60**2\
@@ -80,23 +115,37 @@ class Encoder(object):
 
     @property
     def status(self):
-        value = self.frame / (self.duration * self.frame_rate)
-        if value > 1:
-            value = 1.0
-        return value
+        """
+        Returns output of ffmpeg -i command, providing basic ffmpeg metadata.
+        Can be optimized and improved, but works for now.
+        """
+        try:
+            value = self.frame / (self.duration * self.frame_rate)
+            if value > 1:
+                value = 1.0
+            return value
+        except:
+            return 0.0
 
     @staticmethod
     def _get_ffmpeg_info(path):
+        """
+        Returns output of ffmpeg -i command, providing basic ffmpeg metadata.
+        Can be optimized and improved, but works for now.
+        """
         process = subprocess.Popen([config.FFMPEG_PATH, "-i", path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout, _ = process.communicate()
         result = stdout.decode()
         return result
 
     def _get_print_info(self):
+        """
+        Returns console-formatted table showing essential values to end user.
+        """
         table_data = [
             [clrz('source', 'cyan'), clrz(self.original, 'white')],
             [clrz('output', 'cyan'), clrz(self.output, 'white')],
-            [clrz('preset', 'cyan'), clrz(self.preset, 'magenta')],
+            [clrz('preset', 'cyan'), clrz(self.preset_name, 'magenta')],
             [clrz('duration', 'cyan'), clrz(str(self.duration), 'red')],
             [clrz('framerate', 'cyan'), clrz(str(self.frame_rate), 'red')],
             [clrz('frames', 'cyan'), clrz(str(int(self.duration * self.frame_rate)), 'red')],
@@ -106,29 +155,37 @@ class Encoder(object):
         return table.table
 
     def __init__(self, dir, preset, file):
+        """
+        Instance constructor for Encoder class.
+        """
         self.frame = 0
         self.original = file
-        self.preset = preset
+        self.preset_name = preset
+        self.preset = config.presets[self.preset_name]
         self.destination = dir
-        self.extension = 'mp4'
         self.ffmpeg_info = self._get_ffmpeg_info(self.original)
 
     def __str__(self):
         return self.filename
 
 
+# Setup command line options using Python Click
 @click.command()
 @click.option('--dir', default=False, help='Destination directory.')
 @click.option('--preset', default='default', help='Destination directory.')
 @click.argument('file')
 def handle(dir, preset, file):
-    # click.echo('Converting {} using preset "{}"'.format(file, preset))
+    # Initialize this_encoder instance
     this_encoder = Encoder(dir, preset, file)
+
+    # Display info table for command line users
     print(this_encoder._get_print_info())
 
+    # Haven't settled on a ProgressBar format yet, so leaving a couple options here
     widgets = [ETA(), ' ', Timer(), Bar('>'), ' ', ' #', Counter(), ' ' , Percentage(), ' ', ReverseBar('<') ]
-    widgets = [ETA(), ' ', ' #', Counter(), ' of {}'.format(this_encoder.frames) , '  ', Percentage(), Bar('>'),  ReverseBar('<') ]
+    widgets = [ETA(), ' ', ' #', Counter(), ' of {}'.format(this_encoder.frames) , '  ', Percentage(), Bar('>'), ]
 
+    # Initialize ProgressBar as an instance attribute of this_encoder
     with ProgressBar(max_value=this_encoder.frames, widgets=widgets) as this_encoder.progressbar:
         this_encoder.convert()
 
